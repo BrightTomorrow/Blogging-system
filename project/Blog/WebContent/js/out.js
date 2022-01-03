232713af3eeb82a1778c98c815378e647789a784
BrightTomorrow <87421693+BrightTomorrow@users.noreply.github.com>
2022-01-03 20:51:52 +0800

    Delete project/Blog directory

--- project/Blog/WebContent/js/out.js
+++
@@ -1,9 +0,0 @@
-function custom_close(){
-      if(confirm("Are you sure to shut off the system?")){
-          window.opener=null;
-          window.open('','_self');
-          window.close();
-      }
-      else{
-     }
- }
